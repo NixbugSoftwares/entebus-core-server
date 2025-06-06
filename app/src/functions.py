601d76f2bf7b8e9f5826ac8dbec3cefaca787284
Ashin Joseph <ashinnixbugs@gmail.com>
2025-06-06 10:22:43 +0000

    Added helper functions for vendor and operator

--- app/src/functions.py
+++ app/src/functions.py
@@ -12,11 +12,12 @@ from app.src.db import (
     ExecutiveToken,
     ExecutiveRole,
     ExecutiveRoleMap,
-)
-from app.src.db import (
-    ExecutiveToken,
-    ExecutiveRole,
-    ExecutiveRoleMap,
+    OperatorToken,
+    OperatorRole,
+    OperatorRoleMap,
+    VendorToken,
+    VendorRole,
+    VendorRoleMap,
 )
 
 
@@ -76,3 +77,81 @@ def checkExecutivePermission(role: ExecutiveRole, permission: Column) -> bool:
         return True
     else:
         return False
+
+
+def logOperatorEvent(token: OperatorToken, request: dict, data: dict):
+    logDetails = {
+        "_method": request["method"],
+        "_path": request["path"],
+        "_operator_id": token.operator_id,
+        "_app": "Operator",
+    }
+    logDetails.update(data)
+    openobserve.logEvent(logDetails)
+
+
+def getOperatorToken(access_token: str, session: Session) -> OperatorToken | None:
+    current_time = datetime.now(timezone.utc)
+    return (
+        session.query(OperatorToken)
+        .filter(
+            OperatorToken.access_token == access_token,
+            OperatorToken.expires_at > current_time,
+        )
+        .first()
+    )
+
+
+def getOperatorRole(token: OperatorToken, session: Session) -> OperatorRole | None:
+    return (
+        session.query(OperatorRole)
+        .join(OperatorRoleMap, OperatorRole.id == OperatorRoleMap.role_id)
+        .filter(OperatorRoleMap.operator_id == token.operator_id)
+        .first()
+    )
+
+
+def checkOperatorPermission(role: OperatorRole, permission: Column) -> bool:
+    if role and getattr(role, permission.name, False):
+        return True
+    else:
+        return False
+
+
+def getVendorToken(access_token: str, session: Session) -> VendorToken | None:
+    current_time = datetime.now(timezone.utc)
+    return (
+        session.query(VendorToken)
+        .filter(
+            VendorToken.access_token == access_token,
+            VendorToken.expires_at > current_time,
+        )
+        .first()
+    )
+
+
+def getVendorRole(token: VendorToken, session: Session) -> VendorRole | None:
+    return (
+        session.query(VendorRole)
+        .join(VendorRoleMap, VendorRole.id == VendorRoleMap.role_id)
+        .filter(VendorRoleMap.vendor_id == token.vendor_id)
+        .first()
+    )
+
+
+def checkVendorPermission(role: VendorRole, permission: Column) -> bool:
+    if role and getattr(role, permission.name, False):
+        return True
+    else:
+        return False
+
+
+def logVendorEvent(token: VendorToken, request: dict, data: dict):
+    logDetails = {
+        "_method": request["method"],
+        "_path": request["path"],
+        "_vendor_id": token.vendor_id,
+        "_app": "Vendor",
+    }
+    logDetails.update(data)
+    openobserve.logEvent(logDetails)
