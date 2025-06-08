b2572921065c79a6ccef9056dcb94cd8fd92a843
Thinkal V B <thinkalvb@gmail.com>
2025-06-08 13:00:08 +0000

    Implement landmark management API with validation and exception handling

--- app/api/controller.py
+++ app/api/controller.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.api.token_management import executive as TM_executive
 from app.api.token_management import vendor as TM_vendor
 from app.api.token_management import operator as TM_operator
+from app.api import landmark
 
 app_executive = FastAPI()
 app_vendor = FastAPI()
@@ -13,3 +14,7 @@ app_operator.include_router(TM_operator.route_operator)
 app_executive.include_router(TM_operator.route_executive)
 app_vendor.include_router(TM_vendor.route_vendor)
 app_executive.include_router(TM_vendor.route_executive)
+
+app_executive.include_router(landmark.route_executive)
+app_operator.include_router(landmark.route_operator)
+app_vendor.include_router(landmark.route_vendor)
