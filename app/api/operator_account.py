0848fae40782977a452e7c0906d68a68b2f60bc3
Ashin Joseph <ashinnixbugs@gmail.com>
2025-06-17 06:43:37 +0000

    Refactored the class with field

--- app/api/operator_account.py
+++ app/api/operator_account.py
@@ -1,13 +1,11 @@
-from sqlalchemy.orm.session import Session
 from fastapi import (
     APIRouter,
     Depends,
     status,
     Form,
 )
-from typing import Annotated
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pydantic_extra_types.phone_numbers import PhoneNumber
 from pydantic import EmailStr
 
@@ -18,7 +16,6 @@ from app.src.constants import REGEX_USERNAME, REGEX_PASSWORD
 from app.src.db import (
     sessionMaker,
     Operator,
-    OperatorToken,
 )
 from app.src.functions import (
     enumStr,
@@ -37,18 +34,18 @@ route_executive = APIRouter()
 
 
 class CreateFormForOperator(BaseModel):
-    username: Annotated[str, Form(pattern=REGEX_USERNAME, min_length=4, max_length=32)]
-    password: Annotated[str, Form(pattern=REGEX_PASSWORD, min_length=8, max_length=32)]
-    gender: Annotated[GenderType, Form(description=enumStr(GenderType))] = (
-        GenderType.OTHER
+    username: str = Field(Form(pattern=REGEX_USERNAME, min_length=4, max_length=32))
+    password: str = Field(Form(pattern=REGEX_PASSWORD, min_length=8, max_length=32))
+    gender: GenderType = Field(
+        Form(description=enumStr(GenderType), default=GenderType.OTHER)
     )
-    full_name: Annotated[str | None, Form(max_length=32)] = None
-    email_id: Annotated[EmailStr | None, Form(max_length=256)] = None
-    phone_number: Annotated[PhoneNumber | None, Form(max_length=32)] = None
+    full_name: str | None = Field(Form(max_length=32, default=None))
+    email_id: EmailStr | None = Field(Form(max_length=256, default=None))
+    phone_number: PhoneNumber | None = Field(Form(max_length=32, default=None))
 
 
 class CreateFormForExecutive(CreateFormForOperator):
-    company_id: Annotated[int, Form()]
+    company_id: int = Field(Form())
 
 
 ## API endpoints [Operator]
@@ -113,6 +110,61 @@ async def create_operator(
 
 
 ## API endpoints [Executive]
-@route_executive.post("/company/account", tags=["Operator Account"])
-async def create_operator(bearer=Depends(bearer_executive)):
-    pass
+@route_executive.post(
+    "/company/account",
+    tags=["Operator Account"],
+    response_model=schemas.Operator,
+    status_code=status.HTTP_201_CREATED,
+    responses=makeExceptionResponses(
+        [
+            exceptions.InvalidToken,
+            exceptions.NoPermission,
+        ]
+    ),
+    description="""
+    Creates a new operator account with an active status.
+
+    - Only executive with `create_operator` permission can create operator.
+    - Logs the executive account creation activity with the associated token.
+    - Follow patterns for smooth creation of username and password.
+    - Phone number must follow RFC3966 format.
+    - Email ID must follow RFC5322 format.
+    """,
+)
+async def create_operator(
+    formParam: CreateFormForExecutive = Depends(),
+    bearer=Depends(bearer_executive),
+    request_info=Depends(getRequestInfo),
+):
+    try:
+        session = sessionMaker()
+        token = getExecutiveToken(bearer.credentials, session)
+        if token is None:
+            raise exceptions.InvalidToken()
+        role = getExecutiveRole(token, session)
+        canCreateOperator = bool(role and role.create_operator)
+        if not canCreateOperator:
+            raise exceptions.NoPermission()
+
+        password = argon2.makePassword(formParam.password)
+        operator = Operator(
+            company_id=formParam.company_id,
+            username=formParam.username,
+            password=password,
+            gender=formParam.gender,
+            full_name=formParam.full_name,
+            phone_number=formParam.phone_number,
+            email_id=formParam.email_id,
+        )
+        session.add(operator)
+        session.commit()
+        logExecutiveEvent(
+            token,
+            request_info,
+            jsonable_encoder(operator, exclude={"password"}),
+        )
+        return operator
+    except Exception as e:
+        exceptions.handle(e)
+    finally:
+        session.close()
