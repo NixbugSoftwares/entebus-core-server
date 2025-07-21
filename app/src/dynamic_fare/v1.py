27f4ec0876e7070d6cea0f981005bfe116e2a4f0
Ashin Joseph <ashinnixbugs@gmail.com>
2025-07-21 05:46:07 +0000

    Removed unwanted parameter and added self for reuse js code

--- app/src/dynamic_fare/v1.py
+++ app/src/dynamic_fare/v1.py
@@ -5,28 +5,25 @@ from app.src.constants import TIMEOUT_LIMIT, MAX_MEMORY_SIZE
 # Load the JS function into memory
 # This can also be used to check if the JS code is proper
 class DynamicFare:
-    def validate(jsCode, ticketType, totalDistance) -> bool:
+    def __init__(self, jsCode):
+        self.jsCode = jsCode
+        self.jsContext = None
+
+    def validate(self) -> bool:
         try:
-            jsContext = MiniRacer()
-            jsContext.eval(jsCode, timeout=TIMEOUT_LIMIT, max_memory=MAX_MEMORY_SIZE)
-            jsContext.call(
-                "getFare",
-                ticketType,
-                totalDistance,
-                timeout=TIMEOUT_LIMIT,
-                max_memory=MAX_MEMORY_SIZE,
+            self.jsContext = MiniRacer()
+            self.jsContext.eval(
+                self.jsCode, timeout=TIMEOUT_LIMIT, max_memory=MAX_MEMORY_SIZE
             )
+            # Check if getFare is a function in the JS context
+            fareFunction = self.jsContext.eval("typeof getFare === 'function';")
+            if not fareFunction:
+                self.jsContext = None
+                return False
             return True
         except Exception as e:
+            self.jsContext = None
             return False
 
-    def evaluate(jsCode, ticketType, totalDistance) -> float:
-        jsContext = MiniRacer()
-        jsContext.eval(jsCode, timeout=TIMEOUT_LIMIT, max_memory=MAX_MEMORY_SIZE)
-        return jsContext.call(
-            "getFare",
-            ticketType,
-            totalDistance,
-            timeout=TIMEOUT_LIMIT,
-            max_memory=MAX_MEMORY_SIZE,
-        )
+    def evaluate(self, ticketType, totalDistance) -> float:
+        return self.jsContext.call("getFare", ticketType, totalDistance)
