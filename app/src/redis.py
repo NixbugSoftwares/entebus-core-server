57a020fee18e205c6c8cbbc6b095c1ac0fd25370
Ashin Joseph <ashinnixbugs@gmail.com>
2025-07-22 08:31:20 +0000

    Reframed function for debugging

--- app/src/redis.py
+++ app/src/redis.py
@@ -4,7 +4,7 @@ from redis.lock import Lock
 
 from app.src.constants import REDIS_HOST, REDIS_PORT
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+redisClient = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 
 def acquireLock(tableName: str, pk: Optional[int] = None) -> Optional[Lock]:
@@ -12,16 +12,27 @@ def acquireLock(tableName: str, pk: Optional[int] = None) -> Optional[Lock]:
     Acquire a mutex lock for a table or a specific row using Redis.
     Returns the lock object if acquired, else None.
     """
-    lockName = f"lock:{tableName}" + (f":{pk}" if pk else "")
-    lock = redis_client.lock(lockName)
-    acquired = lock.acquire(blocking=True)
-    return lock if acquired else None
+    try:
+        if pk is None:
+            lockName = f"lock:{tableName}"
+        else:
+            lockName = f"lock:{tableName}:{pk}"
+        lock = redisClient.lock(lockName)
+        acquired = lock.acquire(blocking=True)
+        if acquired:
+            return lock
+        else:
+            return None
+    except Exception:
+        print(f"Failed to acquire lock:")
 
 
 def releaseLock(lock: Lock):
     """
     Release a previously acquired Redis lock.
     """
-    if lock:
-        lock.release()
-    redis_client.close()
+    try:
+        if lock:
+            lock.release()
+    except Exception:
+        print(f"Failed to release lock:")
