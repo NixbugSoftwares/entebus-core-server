3d763f583726978cd600580d08ebb80e78a2cd9d
Ashin Joseph <ashinnixbugs@gmail.com>
2025-07-31 11:00:27 +0000

    Revert "Remove lock if route is none in service"
    This reverts commit 1a568b161edf5be88bc5a3aabfd52ec564b198e3.

--- app/api/service.py
+++ app/api/service.py
@@ -384,7 +384,6 @@ async def create_service(
             raise exceptions.UnknownValue(Service.bus_id)
         route = session.query(Route).filter(Route.id == fParam.route).first()
         if route is None:
-            releaseLock(routeLock)
             raise exceptions.UnknownValue(Service.route)
         fare = session.query(Fare).filter(Fare.id == fParam.fare).first()
         if fare is None:
@@ -656,7 +655,6 @@ async def create_service(
             .first()
         )
         if route is None:
-            releaseLock(routeLock)
             raise exceptions.UnknownValue(Service.route)
         fare = session.query(Fare).filter(Fare.id == fParam.fare).first()
         if fare is None:
