57e356a002676348cc842e2e9583dfe56c2eef94
Ashin Joseph <ashinnixbugs@gmail.com>
2025-07-31 04:48:44 +0000

    Remove lock if route is none in  landmark in route

--- app/api/landmark_in_route.py
+++ app/api/landmark_in_route.py
@@ -251,6 +251,7 @@ async def create_landmark_in_route(
         routeLock = acquireLock(Route.__tablename__, fParam.route_id)
         route = session.query(Route).filter(Route.id == fParam.route_id).first()
         if route is None:
+            releaseLock(routeLock)
             raise exceptions.UnknownValue(LandmarkInRoute.route_id)
         landmarkInRoute = createLandmarkInRoute(session, route, fParam)
         session.add(landmarkInRoute)
@@ -495,6 +496,7 @@ async def create_landmark_in_route(
             .first()
         )
         if route is None:
+            releaseLock(routeLock)
             raise exceptions.UnknownValue(LandmarkInRoute.route_id)
         landmarkInRoute = createLandmarkInRoute(session, route, fParam)
         session.add(landmarkInRoute)
