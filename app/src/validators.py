a6eae68e6ce34cf88aaf2bd26d7e9a547fffe883
Ashin Joseph <ashinnixbugs@gmail.com>
2025-08-12 05:52:00 +0000

    Reframed code structure

--- app/src/validators.py
+++ app/src/validators.py
@@ -166,6 +166,10 @@ def landmarkInRoute(route: int, session: Session) -> bool:
 
 
 def fareFunction(function, attributes) -> str:
+    dfVersion = attributes["df_version"]
+    if dfVersion != DYNAMIC_FARE_VERSION:
+        raise exceptions.InvalidFareVersion()
+
     fareFunction = v1.DynamicFare(function)
 
     ticketTypes = attributes["ticket_types"]
@@ -182,7 +186,3 @@ def fareFunction(function, attributes) -> str:
     totalFareFor0m = fareFunction.evaluate(newTicketTypeName, 0)
     if totalFareFor0m != -1.0:
         raise exceptions.InvalidFareFunction()
-
-    dfVersion = attributes["df_version"]
-    if dfVersion != DYNAMIC_FARE_VERSION:
-        raise exceptions.InvalidFareVersion()
